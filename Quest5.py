9f95122c639a144748293f5141a1128f09165386
Korkor Ndede Kojo <k.ndedekojo@alustudent.com>
2026-08-26 13:35:20 +0400

    Added Quest 5: Magic Calculator

---
+++ Quest5.py
@@ -0,0 +1,16 @@
+# This function checks the person's age category.
+def age_category(age):
+    if age <= 12:
+        print("Child")
+    elif age <= 19:
+        print("Teenager")
+    elif age <= 59:
+        print("Adult")
+    else:
+        print("Senior")
+
+# This asks the user to enter their age.
+age = int(input("Enter your age: "))
+
+# This calls the function.
+age_category(age)
